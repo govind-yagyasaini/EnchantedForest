82e41c40a7fbe029d1eb6bda59ff13b6d8958afe
Govind Yagyasaini <govind.yagyasaini@outlook.com>
2024-10-09 15:03:05 +0530

    Commit

--- GoLang/DataStructures/LinkedList/KthNodeFromLast/main.go
+++ GoLang/DataStructures/LinkedList/KthNodeFromLast/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to finding K-th node from last of a Linked List.")
-	k := 1
+	k := 5
 	myList := LinkedList{}
 	myList.InsertAtEnd(10)
 	myList.InsertAtEnd(20)
@@ -12,8 +12,9 @@ func main() {
 	myList.InsertAtEnd(40)
 	myList.InsertAtEnd(50)
 	myList.DisplayLinkedList()
-	data := myList.KthNodeFromLast(k)
-	fmt.Printf("%d-th node from the end is: %d\n", k, data.data)
+	myList.KthNodeFromLast(k)
+	myList.FindKthNodeFromLast(k)
+	myList.FindKthNodeFromEnd(k)
 }
 
 type Node struct {
@@ -25,9 +26,8 @@ type LinkedList struct {
 	head *Node
 }
 
-// Find k-th node from the end
-func (list *LinkedList) KthNodeFromLast(k int) *Node {
-
+// Find k-th node from the end: Two-pass traversal approach O(n) Complexity
+func (list *LinkedList) KthNodeFromLast(k int) {
 	n := 1
 	temp := list.head
 	for temp.next != nil {
@@ -35,9 +35,9 @@ func (list *LinkedList) KthNodeFromLast(k int) *Node {
 		n++
 	}
 
-	if n < k-1 {
+	if n < k {
 		fmt.Println("There are fewer number of nodes in the list.")
-		return nil
+		return
 
 	}
 	temp = list.head
@@ -45,8 +45,61 @@ func (list *LinkedList) KthNodeFromLast(k int) *Node {
 		temp = temp.next
 
 	}
-	return temp
+	fmt.Printf("%d-th node from the end is: %d\n", k, temp.data)
 
+}
+
+// Find k-th node from the end using Hash Table- O(n) Complexity
+func (list *LinkedList) FindKthNodeFromLast(k int) {
+	if list.head == nil {
+		fmt.Println("List is empty.")
+		return
+	}
+	flag := make(map[int]int)
+	n := 1
+	temp := list.head
+	for temp != nil {
+		flag[n] = temp.data
+		temp = temp.next
+		n++
+	}
+	if n-1 < k {
+		fmt.Println("There are fewer number of nodes in the list.")
+		return
+
+	}
+	fmt.Printf("%d-th node from the end is: %d\n", k, flag[n-k])
+
+}
+
+// Find k-th node from the end using two pointer approach- single pass solution- O(n) complexity
+func (list *LinkedList) FindKthNodeFromEnd(k int) {
+	if list.head == nil {
+		fmt.Println("List is empty.")
+		return
+	}
+	if k <= 0 {
+		fmt.Println("Invalid value for k.")
+		return
+	}
+
+	first := list.head
+	second := list.head
+
+	// Move first pointer k steps ahead
+	for i := 1; i < k; i++ {
+		if first.next == nil {
+			fmt.Printf("The list has fewer than %d elements.\n", k)
+			return
+		}
+		first = first.next
+	}
+
+	for first.next != nil {
+		first = first.next
+		second = second.next
+	}
+	fmt.Printf("%d-th node from the end is: %d\n", k, second.data)
 }
 
 // Insert nodes from end of a linked list
